2394a945ff8e39bcbd878e95cb47a8be2d39bf35
2Hasan2 <h01554176846@gmail.com>
2024-05-24 15:48:07 +0300

    db switch

--- models/__init__.py
+++ models/__init__.py
@@ -8,11 +8,11 @@ from os import getenv
 
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
-# if storage_t == "db":
-# # i commented this out because, there is error in the  DBStorage class 😎
-#     from models.engine.db_storage import DBStorage
-#     storage = DBStorage()
-# else:
-from models.engine.file_storage import FileStorage
-storage = FileStorage()
+if storage_t == "db":
+# i commented this out because, there is error in the  DBStorage class 😎
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
