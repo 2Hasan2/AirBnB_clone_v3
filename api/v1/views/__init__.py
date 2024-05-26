feb4b4a23b9b8525a82edf84e39ddaabddaa8ed7
2Hasan2 <h01554176846@gmail.com>
2024-05-26 14:02:25 +0300

    :)

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,4 +12,3 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-# in the future, import all views here
