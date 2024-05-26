da48f53da883bb12171800d6d130475efbff3e6f
2Hasan2 <h01554176846@gmail.com>
2024-05-26 12:31:29 +0300

    donestyleforapp2

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ Flask Application """
 from api.v1.views import app_views
 from models import storage
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request, abort
 from os import getenv
 from flask_cors import CORS
 
@@ -23,6 +23,11 @@ def not_found(error):
     """ Error 404 """
     return make_response(jsonify({'error': "Not found"}), 404)
 
+@app.before_request
+def check_content_type():
+    """ Check Content Type """
+    if request.content_type and request.content_type != 'application/json':
+        abort(400, description="Content-Type must be application/json")
 
 if __name__ == "__main__":
     """ Main Function """
